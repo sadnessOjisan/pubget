c9d8e01716a151cdbb098216be064fe5a0d8432d
sadness_ojisan <sadness.ojisan@gmail.com>
2019-11-30 12:36:12 +0900

    update: json

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const yargs = require("yargs");
 const react = require("react");
 
+const names = [];
 const resolveObject = obj => {
   for (key in obj) {
-    const t = typeof obj[key];
-    if (t === "function") {
-      const v = obj[key];
-      console.log(v);
-    } else if (t === "object") {
-      const v = obj[key];
-      resolveObject(v);
+    names.push(key);
+    if (key === "constructor") {
+    } else {
+      const t = typeof obj[key];
+      if (t === "function") {
+        const v = obj[key].prototype;
+        resolveObject(v);
+      }
     }
   }
 };
 
 resolveObject(yargs);
+
+const result = Array.from(new Set(names));
+const resultMap = {};
+for (res in result) {
+  resultMap[result[res]] = "";
+}
+
+console.log(resultMap);
