bf6929c04c51c185cde15a627099c73a5970bbf3
sadness_ojisan <sadness.ojisan@gmail.com>
2019-11-30 12:21:58 +0900

    show react func

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const yargv = require("yargs").argv;
+const yargs = require("yargs");
+const react = require("react");
 
-console.log(yargv.x);
+const resolveObject = obj => {
+  for (key in obj) {
+    const t = typeof obj[key];
+    if (t === "function") {
+      const v = obj[key];
+      console.log(v);
+    } else if (t === "object") {
+      const v = obj[key];
+      resolveObject(v);
+    }
+  }
+};
+
+resolveObject(yargs);
